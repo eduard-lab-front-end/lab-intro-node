734f16c01ee09d972dbc7c408ce764d23b742688
eduard-lab-front-end <eduard-lab@web.de>
2024-09-16 12:02:20 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    let start = 0;
+    let end = this.items.length
 
-  get(pos) {}
+    while(start < end) {
+      let mid = Math.floor((start + end) / 2);
+      if(this.items[mid] < item) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+    this.items.splice(start, 0, item);
+    this.length = this.items.length
+  }
+  
+  get(pos) {
+    if(pos < 0 || pos >= this.items.length) throw new Error('OutOfBounds')
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length == 0) throw new Error("EmptySortedList");
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if(this.items.length == 0) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length == 0) return 0;
+    return this.items.reduce((currItem, acc) => acc + currItem, 0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length == 0) throw new Error("EmptySortedList");
+    return this.items.reduce((currItem, acc) => acc + currItem, 0) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
+
